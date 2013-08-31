4b4729a12c10faf1d191150a3caf8aee1a646344
Andrew Hancock <Ah799592@wcupa.edu>
2013-08-31 14:20:23 -0400

    First commit with "Git Extensions" tool.

--- src/slpinterpreter/interp.java
+++ src/slpinterpreter/interp.java
@@ -2,13 +2,19 @@ package slpinterpreter;
 
 class interp {
 
-    static void interp(Stm s) { /* you write this part */ }
+    static void interp(Stm s) 
+    { 
+    	/* you write this part */ 
+    }
 
-    static int maxargs(Stm s) { /* you write this part */
+    static int maxargs(Stm s) 
+    { 
+    	/* you write this part */
         return 0;
     }
 
-    public static void main(String args[]) throws java.io.IOException {
+    public static void main(String args[]) throws java.io.IOException 
+    {
         System.out.println(maxargs(prog.prog));
         interp(prog.prog);
     }
